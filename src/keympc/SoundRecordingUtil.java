1e2c541c239388e5b2fa82478c7c1e3951a587f4
canhnht <canhnhtse03860@fpt.edu.vn>
2015-05-02 21:28:57 -0800

    Complete functions and attributes

--- src/keympc/SoundRecordingUtil.java
+++ src/keympc/SoundRecordingUtil.java
@@ -5,10 +5,17 @@
  */
 package keympc;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
 
 /**
  *
@@ -16,23 +23,75 @@ import javax.sound.sampled.LineUnavailableException;
  */
 public class SoundRecordingUtil {
     
+    // Maximal number of bytes per reading
+    private static final int BUFFER_SIZE = 4096;
+    
+    // All recorded bytes
+    private ByteArrayOutputStream recordedBytes;
+    
+    // Object connected to audio input hardware
+    private TargetDataLine audioLine;
+    
+    // Output audio format
+    private AudioFormat audioFormat;
+    
+    // Check if recorder is running
+    private boolean isRunning;
+    
     // Default constructor
     public SoundRecordingUtil() {        
     }
     
     AudioFormat getAudioFormat() {
-        return null;
+        float rate = 44100;
+        int sizeInBits = 16;
+        int channels = 2;
+        boolean signed = true;
+        boolean bigEndian = true;
+        return new AudioFormat(rate, sizeInBits, channels, signed, bigEndian);
     }
     
     public void start() throws LineUnavailableException {
+        audioFormat = getAudioFormat();
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, audioFormat);
         
+        // check if the system supports specified audio format
+        if (!AudioSystem.isLineSupported(info)) {
+            throw new LineUnavailableException(
+                    "System does not support specified format!");
+        }
+        
+        audioLine = AudioSystem.getTargetDataLine(audioFormat);
+        audioLine.open(audioFormat);
+        audioLine.start();
+        
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead = 0;
+        
+        recordedBytes = new ByteArrayOutputStream();
+        isRunning = true;
+        while (isRunning) {
+            bytesRead = audioLine.read(buffer, 0, buffer.length);
+            recordedBytes.write(buffer, 0, bytesRead);
+        }
     }
     
     public void stop() throws IOException {
-        
+        isRunning = false;
+        if (audioLine != null) {
+            audioLine.drain();
+            audioLine.close();
+        }
     }
     
     public void save(File wavFile) throws IOException {
-        
+        byte[] audioData = recordedBytes.toByteArray();
+        ByteArrayInputStream bais = new ByteArrayInputStream(audioData);
+        AudioInputStream audioInputStream = new AudioInputStream(bais, 
+                audioFormat,
+                audioData.length / audioFormat.getFrameSize());
+        AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, wavFile);
+        audioInputStream.close();
+        recordedBytes.close();
     }
 }
